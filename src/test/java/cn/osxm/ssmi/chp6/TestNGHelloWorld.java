fb3f531c57715686fb33eb657df4015fd38c3a23
osxm <oscar999@mail.ustc.edu.cn>
2018-12-14 06:35:56 +0800

    test

---
+++ src/test/java/cn/osxm/ssmi/chp6/TestNGHelloWorld.java
@@ -0,0 +1,20 @@
+package cn.osxm.ssmi.chp6;
+
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class TestNGHelloWorld {
+	private int iNo = 0;
+
+	@BeforeClass
+	public void mySetUp() {
+		// "Initial Some ...."
+		iNo = 1;
+	}
+
+	@Test()
+	public void tStr() {
+		Assert.assertEquals(iNo, 1);
+	}
+}
