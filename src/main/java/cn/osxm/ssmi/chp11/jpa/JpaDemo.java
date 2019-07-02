3297bdaf95b74ed90623a6643ef7b8c6169c002b
osxm <oscar999@mail.ustc.edu.cn>
2019-07-02 22:09:09 +0800

    chp11

--- src/main/java/cn/osxm/ssmi/chp11/jpa/JpaDemo.java
+++ src/main/java/cn/osxm/ssmi/chp11/jpa/JpaDemo.java
@@ -15,44 +15,46 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
-import cn.osxm.ssmi.chp02.User;
+import cn.osxm.ssmi.chp11.User;
 
 /**
-  * @ClassName: JpaDemo
-  * @Description: TODO
-  * @author osxm:oscarxueming
-  */
+ * @ClassName: JpaDemo
+ * @Description: TODO
+ * @author osxm:oscarxueming
+ */
 
 public class JpaDemo {
 
 	/**
-	  * @Title: main
-	  * @Description: TODO
-	  * @param args
-	  */
+	 * @Title: main
+	 * @Description: TODO
+	 * @param args
+	 */
 
 	public static void main(String[] args) {
-		 EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("ssmi");
-	        EntityManager entityManager = entityManagerFactory.createEntityManager();
-	        entityManager.getTransaction().begin();
+		EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("ssmi");
+		EntityManager entityManager = entityManagerFactory.createEntityManager();
+		entityManager.getTransaction().begin();
 
-	        List<User> result = entityManager.createQuery("select u " + "from User u ").getResultList();
-	        //List<User> result = entityManager.createNamedQuery("User.findAll").getResultList();
-	        for (User user : result) {
-	            System.out.println(user.getName());
-	        }
-	        
-	        //注解方式
-	      /* List<UserAnnoEntity> result2 = entityManager.createQuery("select u " + "from user u ").getResultList();
-	        for (UserAnnoEntity user : result2) {
-	            System.out.println(user.getName());
-	        }*/
-	        
-	        
-	        entityManager.getTransaction().commit();
-	        entityManager.close();
+		// User user = entityManager.find(User.class, 1);
+		// System.out.println(user.getName());
+		List<User> result = entityManager.createQuery("select u " + "from User u ").getResultList();
+		// List<User> result =
+		// entityManager.createNamedQuery("User.findAll").getResultList();
+		for (User user : result) {
+			System.out.println(user.getName());
+		}
 
-	    }
-	
+		// 注解方式
+		/*
+		 * List<UserAnnoEntity> result2 = entityManager.createQuery("select u " +
+		 * "from user u ").getResultList(); for (UserAnnoEntity user : result2) {
+		 * System.out.println(user.getName()); }
+		 */
+
+		entityManager.getTransaction().commit();
+		entityManager.close();
+
+	}
 
 }
